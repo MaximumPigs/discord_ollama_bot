6251e6c12a6e3d1dfce2b5115ced68feaed33feb
Max <98137611+MaximumPigs@users.noreply.github.com>
2025-02-05 08:56:17 +1100

    Major changes
    Rewrite functions and combine chat and image.
    Add logging.
    Add some error handling.

--- discord_ollama.py
+++ discord_ollama.py
@@ -6,11 +6,23 @@ from requests import get
 from PIL import Image
 from io import BytesIO
 import base64
+import logging
+from uuid import uuid4
+from time import time
+
+### GET ENVIRONMENT VARIABLES
 
 discord_token = environ['DISCORD_TOKEN']
 ollama_host = environ['OLLAMA_HOST']
-ollama_model = environ['OLLAMA_MODEL']
+chat_model = environ['CHAT_MODEL']
 image_model = environ['IMAGE_MODEL']
+
+### SET UP LOGGING
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
+logger = logging.getLogger('bot')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 ### SET UP OLLAMA
 
@@ -25,97 +37,134 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 chunk_length = 1500
 
+
+async def on_command_error(ctx, error):
+    await ctx.send(f'An error occurred: {error}')
+
 def response_split(response):
+    """
+    Determines if response is too long and must be split.
+    :param response: A list of response bits (words).
+    :return: Boolean.
+    """
     res = ''.join(response)
     if len(res) > chunk_length and res[-1] == ".":
         return True
 
-def get_image_base64(url):
+
+def get_images_from_attachments(attachments: list[Attachment]):
+    """
+    Returns a list of base64 encoded images from a list of attachments.
+    Will only convert attachments with a file type of "image"
+    :param attachments: A list of discord Attachment objects.
+    :return: A list of base64 encoded images.
+    """
+    images = []
+    for attachment in attachments:
+        content_type = attachment.content_type
+        file_type = content_type.split('/')[0]
+        sub_type = content_type.split('/')[1]
+        if file_type == 'image':
+            images.append(get_image_base64(attachment.url, sub_type))
+    return images
+
+
+def get_image_base64(url, fmt):
+    """
+    Returns a base64 encoded image from a URL.
+    Stores file in a buffer as to not write anything to disk.
+    :param url: A URL obtained from a discord Attachment object.
+    :param fmt: The image format obtained from a discord Attachment object.
+    :return: A base64 string of the image.
+    """
     buffered = BytesIO()
     r = get(url, stream=True)
     r.raw.decode_content = True
     im = Image.open(r.raw)
-    im.save(buffered, format='JPEG')
+    im.save(buffered, format=fmt)
     return base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-def convert_to_b64(url):
-    pass
 
 @bot.command()
-async def chat(ctx, *args, think=False):
-    input_str = ' '.join(args)
-    message = {'role': 'user',
-                'content':input_str
-               }
+async def chinabot(ctx, *args, think = False):
+
+    uuid = uuid4()
+    bits = 0
+    chunks = 0
     response = []
+    start = time()
+    input_str = ' '.join(args)
+    attachments = ctx.message.attachments
     thinking = False
 
-    async for part in await ollama_client.chat(model=ollama_model, messages=[message], stream=True):
+    logger.info(f"Command Invocation - uuid: '{uuid}', "
+                f"user: '{ctx.message.author}', "
+                f"command: '{ctx.command}', "
+                f"argument: '{input_str}', "
+                f"attachment_count: '{len(attachments)}'")
+
+    for attachment in attachments:
+        logger.debug(f"Attachment Uploaded - uuid: '{uuid}', "
+                     f"user: '{ctx.message.author}', "
+                     f"id: '{attachment.id}', "
+                     f"filename: '{attachment.filename}', "
+                     f"size: '{attachment.size}', "
+                     f"content_type: '{attachment.content_type}')")
+
+    images = get_images_from_attachments(attachments)
+
+    if len(images) > 0:
+        model = image_model
+    else:
+        model = chat_model
+
+    logger.debug(f"Model Selection - uuid: '{uuid}', model: '{model}'")
+
+    message = {'role': 'user',
+               'content': input_str,
+               'images': images}
+
+    await ctx.typing()
+
+    async for part in await ollama_client.chat(model=model, messages=[message], stream=True):
+
+        await ctx.typing()
+
+        bits += 1
         bit = part['message']['content']
         done = part['done']
         if bit == '<think>':
-            #print("I'm thinking")
+            logger.debug(f"Think Block - uuid: '{uuid}', message: 'Start'")
             thinking = True
 
-        if think and thinking:
-            #print("I'm writing my thoughts")
-            response.append(bit)
-            if bit == '\n\n' or done or response_split(response):
-                chunk = "".join(response)
-                if len(chunk) > 0 and not chunk == "\n\n":
-                    await ctx.send(chunk)
-                response = []
+            if think:
+                logger.debug(f"Think Block - uuid: '{uuid}', message: 'Writing'")
 
-        elif not thinking:
-            #print("I'm not thinking")
+            else:
+                logger.debug(f"Think Block - uuid: '{uuid}', message: 'Skipping'")
+
+        if (think and thinking) or (not thinking):
             response.append(bit)
             if bit == '\n\n' or done or response_split(response):
                 chunk = "".join(response)
+                chunks += 1
                 if len(chunk) > 0 and not chunk == "\n\n":
                     await ctx.send(chunk)
                 response = []
 
         if bit == '</think>':
-            #print("I'm finished thinking")
+            logger.debug(f"Think Block - uuid: '{uuid}', message: 'End'")
             thinking = False
 
-@bot.command()
-async def image(ctx, attachment: Attachment, *args, think=False):
-    input_str = ' '.join(args)
-    message = {'role': 'user',
-               'content':input_str,
-               'images': [get_image_base64(attachment)]
-               }
-    response = []
-    thinking = False
+        if done:
+            end = time()
+            duration = round(end - start,2)
+            logger.info(f"Command Completion - uuid: '{uuid}', "
+                        f"user: '{ctx.message.author}', "
+                        f"command: '{ctx.command}', "
+                        f"duration_s: '{duration}' "
+                        f"bits: {bits}, "
+                        f"chunks: {chunks}")
 
-    async for part in await ollama_client.chat(model=image_model, messages=[message], stream=True):
-        bit = part['message']['content']
-        done = part['done']
-        if bit == '<think>':
-            #print("I'm thinking")
-            thinking = True
-
-        if think and thinking:
-            #print("I'm writing my thoughts")
-            response.append(bit)
-            if bit == '\n\n' or done or response_split(response):
-                chunk = "".join(response)
-                if len(chunk) > 0 and not chunk == "\n\n":
-                    await ctx.send(chunk)
-                response = []
-
-        elif not thinking:
-            #print("I'm not thinking")
-            response.append(bit)
-            if bit == '\n\n' or done or response_split(response):
-                chunk = "".join(response)
-                if len(chunk) > 0 and not chunk == "\n\n":
-                    await ctx.send(chunk)
-                response = []
-
-        if bit == '</think>':
-            #print("I'm finished thinking")
-            thinking = False
-
-bot.run(discord_token)
+bot.on_command_error = on_command_error
+bot.run(discord_token, log_handler=handler)
